0cac8852b789adf391087056ecaab27ad266e9d9
Lee Langley-Rees <lee@greenimp.co.uk>
2019-12-17 12:53:06 +0000

    Starting on Roll Groups

--- src/RollGroup.js
+++ src/RollGroup.js
@@ -1,3 +1,9 @@
+import ComparePoint from './ComparePoint';
+import ExplodeModifier from './modifiers/ExplodeModifier';
+import Modifier from './modifiers/Modifier';
+import ReRollModifier from './modifiers/ReRollModifier';
+import RollResult from './results/RollResult';
+
 const modifiersSymbol = Symbol('modifiers');
 const notationSymbol = Symbol('notation');
 const expressionsSymbol = Symbol('expressions');
@@ -10,18 +16,92 @@ class RollGroup {
    * @param {[]|null} modifiers
    */
   constructor(notation, expressions, modifiers = null) {
+    if (!notation) {
+      throw new TypeError('Notation is required');
+    }
+
     this[notationSymbol] = notation;
-    this[expressionsSymbol] = expressions;
-    this[modifiersSymbol] = modifiers;
+    this.expressions = expressions || [];
+    this.modifiers = modifiers || [];
+  }
+
+  /**
+   * The expressions in this group
+   *
+   * @returns {[]}
+   */
+  get expressions() {
+    return [...(this[expressionsSymbol] || [])];
+  }
+
+  /**
+   * Sets the expressions
+   *
+   * @param {[]} expressions
+   */
+  set expressions(expressions) {
+    console.log(expressions);
+    if (!expressions || !Array.isArray(expressions)) {
+      throw new Error(`Expressions must be an array: ${expressions}`);
+    }
+
+    // loop through each expression and add it to the list
+    this[expressionsSymbol] = [];
+    expressions.forEach((expression) => {
+      this.addExpression(expression);
+    });
   }
 
   /**
    * The modifiers that affect this group
    *
-   * @returns {Modifier[]|null}
+   * @returns {Map|null}
    */
   get modifiers() {
-    return this[modifiersSymbol];
+    // ensure modifiers are ordered correctly
+    if (this[modifiersSymbol]) {
+      return new Map([...this[modifiersSymbol]].sort((a, b) => a[1].order - b[1].order));
+    }
+
+    return null;
+  }
+
+  /**
+   * Sets the modifiers that affect this roll
+   *
+   * @param value
+   */
+  set modifiers(value) {
+    let modifiers;
+    if (value instanceof Map) {
+      modifiers = value;
+    } else if (Array.isArray(value)) {
+      // loop through and get the modifier name of each item and use it as the map key
+      modifiers = new Map(value.map((modifier) => [modifier.name, modifier]));
+    } else if (typeof value === 'object') {
+      modifiers = new Map(Object.entries(value));
+    } else {
+      throw new Error('modifiers should be a Map or an Object');
+    }
+
+    if (
+      modifiers.size
+      && [...modifiers.entries()].some((entry) => !(entry[1] instanceof Modifier))
+    ) {
+      throw new Error('modifiers is invalid. List must only contain Modifier instances');
+    }
+
+    this[modifiersSymbol] = modifiers;
+
+    // loop through each modifier and ensure that those that require it have compare points
+    // @todo find a better way of defining compare point on modifiers that don't have them
+    this[modifiersSymbol].forEach((modifier) => {
+      if ((modifier instanceof ExplodeModifier) && !modifier.comparePoint) {
+        modifier.comparePoint = new ComparePoint('=', this.max);
+      } else if ((modifier instanceof ReRollModifier) && !modifier.comparePoint) {
+        modifier.comparePoint = new ComparePoint('=', this.min);
+      }
+    });
   }
 
   /**
@@ -33,13 +113,8 @@ class RollGroup {
     return this[notationSymbol];
   }
 
-  /**
-   * The expressions in this group
-   *
-   * @returns {StandardDice[]}
-   */
-  get expressions() {
-    return this[expressionsSymbol];
+  addExpression(value) {
+    this[expressionsSymbol].push(value);
   }
 
   /**
